87cc6f421efddf4b59c6f2601415ec31e76ce9ac
fitstand8 <sukhudyan12@gmail.com>
2021-08-12 22:57:23 +0400

    Corrected the mistake

--- hw12.08.2021/SQLQuery1.sql
+++ hw12.08.2021/SQLQuery1.sql
@@ -1,5 +1,7 @@
+use TSQLV4;
 select top(10) custid, count(*) as orders_count
 from Sales.Orders
 group by custid
 having count(*) > (select avg(counts) from (select custid, count(*) as counts from Sales.Orders group by custid)as a)
+order by count(*) desc;
 
