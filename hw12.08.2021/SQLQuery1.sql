39a4884f4cc7ec69f9e182572b1e36cf56ecea50
fitstand8 <sukhudyan12@gmail.com>
2021-08-12 23:07:28 +0400

    Correcret another mistake

--- hw12.08.2021/SQLQuery1.sql
+++ hw12.08.2021/SQLQuery1.sql
@@ -3,5 +3,5 @@ select top(10) custid, count(*) as orders_count
 from Sales.Orders
 group by custid
 having count(*) > (select avg(counts) from (select custid, count(*) as counts from Sales.Orders group by custid)as a)
-order by count(*) desc;
+order by orders_count desc;
 
