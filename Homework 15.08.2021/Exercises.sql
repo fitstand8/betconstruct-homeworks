c06307f12751da46c7722cc6f0ed69d57825f5df
fitstand8 <sukhudyan12@gmail.com>
2021-08-15 20:05:46 +0400

    Homework 15.08.2021

--- Homework 15.08.2021/Exercises.sql
+++ Homework 15.08.2021/Exercises.sql
@@ -9,11 +9,11 @@ left join Sales.Orders on Sales.Customers.custid = Sales.Orders.custid;
 --Exercise 2 method 1
 select Sales.Customers.custid, Sales.Customers.contactname, Sales.Orders.orderid, Sales.Orders.orderdate 
 from Sales.Customers
-join Sales.Orders on Sales.Customers.custid = Sales.Orders.custid
+inner join Sales.Orders on Sales.Customers.custid = Sales.Orders.custid
 where Sales.Orders.orderdate = '2016-02-12';
 
 
 --Exercise 2 method 2
 select Sales.Customers.custid, Sales.Customers.contactname, Sales.Orders.orderid, Sales.Orders.orderdate 
 from Sales.Customers
-join Sales.Orders on Sales.Customers.custid = Sales.Orders.custid and Sales.Orders.orderdate = '2016-02-12';
+inner join Sales.Orders on Sales.Customers.custid = Sales.Orders.custid and Sales.Orders.orderdate = '2016-02-12';
